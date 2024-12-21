807dd91fe0236f821da94fef26e79234d3d98c45
comjeong <>
2024-12-21 21:03:38 +0900

    feat: test paymentService

--- src/main/java/org/example/study_spring/PaymentService.java
+++ src/main/java/org/example/study_spring/PaymentService.java
@@ -1,13 +1,17 @@
 package org.example.study_spring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        return new Payment(orderId, currency, foreignCurrencyAmount , BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
 }
